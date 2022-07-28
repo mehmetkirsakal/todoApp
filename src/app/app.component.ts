d8a03fccd49fa532db618146719b85521e4c059e
Mehmet <mehmet_kirsakal@hotmail.com>
2022-07-28 23:53:50 +0300

    Model_Binding commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Mehmet';
+
+  items= [
+    {FilmAdi: "Film1", Durum:"No"},
+    {FilmAdi: "Film2", Durum:"No"},
+    {FilmAdi: "Film3", Durum:"No"},
+    {FilmAdi: "Film4", Durum:"No"}
+
+
+  ];
 }
